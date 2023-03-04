97f137a0a2d6a68eb658def70d43a15f7aa79ae7
Artyom Pionov <pionovartem11@gmail.com>
2023-03-04 18:07:10 +0000

    Second Commit

---
+++ src/WordSwapper.java
@@ -0,0 +1,21 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+// Code that uses Pattern and Matcher class
+// to swap words within a phrase.
+// In this case, swapping first name and last name.
+public class WordSwapper {
+    public static void main(String[] args) {
+        // Phrase that will be "edited".
+        String phrase = "Artyom Pionov";
+
+        // Setting up
+        Pattern pattern = Pattern.compile("(.*) (.*)");
+        Matcher matcher = pattern.matcher(phrase);
+
+        // Changing up the first group with second group.
+        System.out.println(matcher.replaceFirst("$2 $1"));
+        // $0 is Artyom Pionov
+        // Final Output: Pionov Artyom
+    }
+}
